9b72d6f47c96fd71f1a00c1d2b3585d47c3311e7
da_m1r <damirr765@gmail.com>
2023-02-18 18:01:34 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const { sendNotify } = require("./telegram");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 //роуты
